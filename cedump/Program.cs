3234cbbb7866ebdeea9bec46c6108911f42503f7
Scott Danley <scott.danley@leggett.com>
2022-11-17 17:07:29 -0600

    Add project files.

---
+++ cedump/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.CommandLine;
+using System.Data.SqlServerCe;
+using System.IO;
+
+namespace cedump
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var directory = new DirectoryInfo(@"C:\System\Control\Data\");
+            var directoryArg = new Argument<DirectoryInfo>("data folder", getDefaultValue: () => directory);
+            var rootCommand = new RootCommand("dump sqlce files as csv") { directoryArg };
+
+            rootCommand.SetHandler((dir) =>
+            {
+                directory = dir;
+            }, directoryArg);
+
+            rootCommand.InvokeAsync(args).Wait();
+
+            foreach (var filename in new[] { "MachineModelsDB.sdf", "CustomerDB.sdf", "QuilterDB.sdf" })
+            {
+                var path = Path.Combine(@"C:\System\Control\Data\", filename);
+
+                var connection = new SqlCeConnection($"Data Source={path}");
+                connection.Open();
+
+                // read each table
+                var command = new SqlCeCommand("select * from information_schema.tables", connection);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var table = reader[2].ToString();
+
+                    // don't dump products table
+                    if (table == "Products")
+                        continue;
+
+                    Console.WriteLine($"#{table}");
+                    command = new SqlCeCommand($"select * from {table}", connection);
+                    var tableReader = command.ExecuteReader();
+
+                    // write header row
+                    int fieldCount = tableReader.FieldCount;
+                    for (var i = 0; i < fieldCount; i++)
+                    {
+                        Console.Write(tableReader.GetName(i));
+                        if (i < fieldCount - 1)
+                            Console.Write(",");
+                    }
+                    Console.WriteLine();
+
+                    // write data as csv
+                    while (tableReader.Read())
+                    {
+                        for (var i = 0; i < fieldCount; i++)
+                        {
+                            Console.Write(tableReader[i]);
+                            if (i < fieldCount - 1)
+                                Console.Write(",");
+                        }
+                        Console.WriteLine();
+                    }
+                    tableReader.Close();
+                }
+                // cleanup
+                reader.Close();
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ ceimport/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Data;
+using System.CommandLine;
+
+var filepath = new FileInfo("../../../../cedump/bin/Debug/cedump.exe");
+var fileArg = new Argument<FileInfo>("data folder", getDefaultValue: () => filepath);
+var rootCommand = new RootCommand("dump sqlce files as csv") { fileArg };
+
+rootCommand.SetHandler((file) =>
+{
+    if (file.Exists)
+    {
+        // use file argument
+        filepath = file;
+    }
+    else
+    {
+        // search parent directory for cedump.exe
+        var found = Directory.GetFiles("../../../../cedump", "cedump.exe", SearchOption.AllDirectories)
+            .FirstOrDefault();
+        if (found is not null)
+        {
+            filepath = new FileInfo(found);
+        }
+        else
+        {
+            // report error and exit
+            Console.WriteLine("cedump.exe not found");
+            Environment.Exit(1);
+        }
+
+    }
+    filepath = file;
+}, fileArg);
+
+System.Diagnostics.Process process = new System.Diagnostics.Process();
+process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+process.StartInfo.FileName = filepath.FullName;
+process.StartInfo.UseShellExecute = false;
+process.StartInfo.CreateNoWindow = true;
+process.StartInfo.RedirectStandardOutput = true;
+process.Start();
+
+DataSet dataset = new();
+DataTable table = new();
+
+// read output and store in stringbuilder
+while (!process.StandardOutput.EndOfStream)
+{
+    var line = process.StandardOutput.ReadLine();
+    if (string.IsNullOrEmpty(line)) continue;
+
+    // parse line and check for #tablename
+    if (line.StartsWith("#"))
+    {
+        table = dataset.Tables.Add(line.Substring(1));
+        line = process.StandardOutput.ReadLine();
+        if (string.IsNullOrEmpty(line)) continue;
+
+        // parse header and add columns to table
+        foreach (var name in line.Split(","))
+        {
+            table.Columns.Add(name, typeof(string));
+        }
+        continue;
+    }
+    // parse data and add row to table
+    var row = table.NewRow();
+    var values = line.Split(",");
+    for (var i = 0; i < values.Length; i++)
+    {
+        row[i] = values[i];
+    }
+    table.Rows.Add(row);
+}
+
+Console.WriteLine(dataset.GetXml());
